1dc4e6ce79dcf9d3b13130b8da3c68a5c9936ffe
David Argote <argote.2075@gmail.com>
2019-08-13 01:18:01 +0200

    mejora juego 2

--- app/src/main/java/com/senasoft/appdoman/control/ui/main/PageViewModel.java
+++ app/src/main/java/com/senasoft/appdoman/control/ui/main/PageViewModel.java
@@ -8,11 +8,16 @@ import androidx.lifecycle.ViewModel;
 
 public class PageViewModel extends ViewModel {
 
+    //Instancia la fuente de datos
+
+    String[] lista = {"Mesa", "Arbol", "Oro", "Pelota", "Juego", };
+
     private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Palabra " + input;
+            return  lista[input-1]; // retorna la palabra
         }
     });
 
