8a8381ccc7a00cfe110b5aeae8d9ee7af48be1ae
CRISTHIAN <criacamu@gmail.com>
2019-08-13 19:33:53 -0500

    Se crea register y se enlaza

--- app/src/main/java/com/senasoft/appdoman/control/Login.java
+++ app/src/main/java/com/senasoft/appdoman/control/Login.java
@@ -2,16 +2,37 @@ package com.senasoft.appdoman.control;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.senasoft.appdoman.R;
 
 public class Login extends AppCompatActivity {
-
+    EditText etUserLogin,etPasswordLogin;
+    Button btnIniciaSesionLog;
+    TextView titleRegisterRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etPasswordLogin = findViewById(R.id.etPasswordLogin);
+        etUserLogin = findViewById(R.id.etUserLogin);
+        titleRegisterRegister = findViewById(R.id.titleRegisterRegister);
+        btnIniciaSesionLog = findViewById(R.id.btnIniciaSesionLog);
+        btnIniciaSesionLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/senasoft/appdoman/control/Register.java
+++ app/src/main/java/com/senasoft/appdoman/control/Register.java
@@ -3,14 +3,33 @@ package com.senasoft.appdoman.control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.senasoft.appdoman.R;
 
 public class Register extends AppCompatActivity {
 
+
+    EditText etUserReg,etUserNameReg,etPasswordRegConfir;
+    Button btnRegistrarReg;
+    RadioButton rButNinioReg,rButNiniaReg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUserReg = findViewById(R.id.etUserReg);
+        etUserNameReg = findViewById(R.id.etUserNameReg);
+        etPasswordRegConfir = findViewById(R.id.etPasswordRegConfir);
+
+        rButNinioReg = findViewById(R.id.rButNinioReg);
+        rButNiniaReg = findViewById(R.id.rButNiniaReg);
+
+        btnRegistrarReg = findViewById(R.id.btnRegistrarReg);
+
     }
 }
